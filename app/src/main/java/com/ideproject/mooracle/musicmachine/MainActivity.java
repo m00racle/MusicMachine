3d3f283610b53ef7b7463b27b83f238fb7f5687f
mooracle <yanuar.prakosa@gmail.com>
2019-04-27 22:22:40 +0700

    Binding Initiated

--- app/src/main/java/com/ideproject/mooracle/musicmachine/MainActivity.java
+++ app/src/main/java/com/ideproject/mooracle/musicmachine/MainActivity.java
@@ -1,6 +1,10 @@
 package com.ideproject.mooracle.musicmachine;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,12 +20,26 @@ public class MainActivity extends AppCompatActivity {
     // And let's set it equal to MainActivity.class.getSimpleName.
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_SONG = "song";
+    private boolean mBound = false; // this will track the connection is bound or not
     //1:53
     //Great, let's switch back to our activity and
     //declare our button as a field at the top of our class.
     //Let's call it mDownloadButton.
     private Button mDownloadButton;
     private Button mPlayerButton;
+
+    //build anonymous class for service connection:
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,5 +81,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, PlayerService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound){
+            unbindService(serviceConnection);
+            mBound = false;
+            //note: unbinding service will not change the mBound since onDisconnected only called in
+            //extraordinary circumstances. Thus this class itself need to change mBound value.
+        }
     }
 }
